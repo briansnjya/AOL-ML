1fc0f2352069b3784ebda3060cd17099fa69b023
briansnjya <kjoy76979@gmail.com>
2026-06-06 14:33:43 +0700

    pindahin path deploy ke huggingface

--- FrontEnd/src/components/SalaryPredictor.jsx
+++ FrontEnd/src/components/SalaryPredictor.jsx
@@ -120,7 +120,7 @@ const SalaryPredictor = () => {
 
     try {
       console.log("Payload dikirim:", payload);
-      const response = await fetch('https://aol-ml-production.up.railway.app/predict', {
+      const response = await fetch('https://briansnjya-deploy-backend.hf.space', {
         method:  'POST',
         headers: { 'Content-Type': 'application/json' },
         body:    JSON.stringify(payload),
